702d1ca91ab57799f08b0fd384b481c9b6d5d874
Sergey Kanzhelev <sergkanz@microsoft.com>
2016-08-04 16:55:06 -0700

    removed generated schemas. Use bond generator

--- EndpointSpecs/Tools/generateSchema.ps1
+++ EndpointSpecs/Tools/generateSchema.ps1
@@ -13,16 +13,16 @@ $schemasPath = "$schemasPath\v2\Bond"
 
 & "$generatorPath\BondSchemaGenerator.exe" -v -i "$schemasPath\AppInsightsTypes.bond" -i "$schemasPath\ContextTagKeys.bond" -o "$currentDir\..\Schemas\Docs\" -e DocsLanguage -t DocsLayout -n test --flatten false
 
-mkdir -Force .\obj
-Invoke-WebRequest -o ".\obj\nuget.exe" https://api.nuget.org/downloads/nuget.exe
+#mkdir -Force .\obj
+#Invoke-WebRequest -o ".\obj\nuget.exe" https://api.nuget.org/downloads/nuget.exe
 
-& .\obj\nuget install Bond.CSharp -Version 4.2.1 -OutputDirectory .\obj\packages
+#& .\obj\nuget install Bond.CSharp -Version 4.2.1 -OutputDirectory .\obj\packages
 
 
-dir "$currentDir\..\Schemas\Bond\" | ForEach-Object { 
-    & .\obj\packages\Bond.CSharp.4.2.1\tools\gbc.exe schema -o "$currentDir\..\Schemas\JSONSchema" $_.FullName
-}
+#dir "$currentDir\..\Schemas\Bond\" | ForEach-Object { 
+#    & .\obj\packages\Bond.CSharp.4.2.1\tools\gbc.exe schema -o "$currentDir\..\Schemas\JSONSchema" $_.FullName
+#}
 
-dir "$currentDir\..\Schemas\JSONSchema\" | ForEach-Object { 
-    Get-Content $_.FullName | ConvertFrom-Json | ConvertTo-Json | Set-Content $_.FullName
-}
+#dir "$currentDir\..\Schemas\JSONSchema\" | ForEach-Object { 
+#    Get-Content $_.FullName | ConvertFrom-Json | ConvertTo-Json | Set-Content $_.FullName
+#}
